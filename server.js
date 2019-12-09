3e9275abfff8a8a98adb85ef91197e7fbfc19f4d
kmyles82 <kmyles82@gmail.com>
2019-12-09 14:42:49 -0600

    installed mongoose

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-
-//Route files
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
 
 //Load env vars
 dotenv.config({
     path: './config/config.env'
 });
+
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express();
 
@@ -22,6 +26,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
 });
+
+//Handle unhandled Promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    //Close server and exit process with a failure
+    server.close(() => process.exit(1))
+})
