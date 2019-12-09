eb25731a88ae75a3965165684a96c337054487a5
kmyles82 <kmyles82@gmail.com>
2019-12-09 12:29:39 -0600

    refactored routes into own folder

--- server.js
+++ server.js
@@ -1,46 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
+
 //Load env vars
 dotenv.config({
     path: './config/config.env'
 });
 
 const app = express();
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
 const PORT = process.env.PORT || 5000
-
-//express routes
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success: true, msg: 'Show all bootcamps'})    
-})
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Show a single bootcamp'
-    })
-})
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Create a new bootcamps'
-    })
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Update a single bootcamp ${req.params.id}`
-    })
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Deleted a single bootcamp ${req.params.id}`
-    })
-})
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
