8e2f51a19892fdf04a09b0f96d27cb4d85397fea
kmyles82 <kmyles82@gmail.com>
2019-12-09 12:22:15 -0600

    added express routes

--- server.js
+++ server.js
@@ -9,7 +9,39 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 5000
 
+//express routes
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Show all bootcamps'})    
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Show a single bootcamp'
+    })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Create a new bootcamps'
+    })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Update a single bootcamp ${req.params.id}`
+    })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Deleted a single bootcamp ${req.params.id}`
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
 });
-
