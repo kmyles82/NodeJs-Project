566acf26496cf4465b190b44a8493740c3177021
kmyles82 <kmyles82@gmail.com>
2019-12-09 17:14:18 -0600

    refactored getBootcamps and getBootcamp to utilize async/await and try/catch

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,26 +5,36 @@ const Bootcamp = require('../models/Bootcamp')
 //@access   Public
 exports.getBootcamps = async (req, res, next) => {
     try {
-        
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, data: bootcamps})
     } catch (err) {
-        
+        res.status(400).json({ success: false})
     }
-    res.status(200).json({
-        success: true,
-        msg: 'Show all bootcamps',
-        hello: req.hello
-    })
 }
 
 
 //@desc     Get a single bootcamps
 //@route    GET /api/v1/bootcamps/:id'
 //@access   Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Show a single bootcamp'
-    })
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        
+        if (!bootcamp) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
 }
 
 //@desc     Create a single bootcamp
