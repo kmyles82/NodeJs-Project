5454b566660a079ea70066b7864b0fc61b1568c8
kmyles82 <kmyles82@gmail.com>
2019-12-10 09:59:03 -0600

    created custom error handler middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
 //Load env vars
@@ -17,13 +18,20 @@ const bootcamps = require('./routes/bootcamps')
 
 const app = express();
 
+//Body parser
+app.use(express.json())
+
+//Middleware
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
+
+//Middleware
+//This middleware must come after routes
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
