de0d7eb7fe1fbe23f10a815f6ad3b0f59ab855b8
kmyles82 <kmyles82@gmail.com>
2019-12-16 11:22:18 -0600

    added populate() to exact specific data and add mongoose virtuals middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -100,7 +100,14 @@ const BootcampSchema = new mongoose.Schema(
       default: Date.now
         }
     }
-);
+  , {
+    toJSON: {
+      virtuals: true
+    },
+    toObject: {
+      virtuals: true
+    }
+});
 
 //Create bootcamp slug from the name
 BootcampSchema.pre('save', function (next) {
@@ -127,6 +134,21 @@ BootcampSchema.pre('save', async function (next) {
   this.address = undefined
   
   next()
+});
+
+//Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`)
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
 })
+
+//Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
