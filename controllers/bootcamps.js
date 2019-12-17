6c0093c30ebc7475b8d3d10cf73eaa20729fd598
kmyles82 <kmyles82@gmail.com>
2019-12-17 16:15:28 -0600

    install express-fileupload, create static public folder, added upload file feature and route

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,6 +2,7 @@ const geoCoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const path = require('path')
 
 //@desc     Get all bootcamps
 //@route    GET /api/v1/bootcamps
@@ -169,3 +170,60 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     })
 })
 
+
+//@desc     Upload photo for bootcamps
+//@route    PUT /api/v1/bootcamps/:id/photo
+//@access   Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404)
+        )
+    }
+
+    if (!req.files) {
+        return next(
+           new ErrorResponse(`Please upload a file`, 400)
+        )
+    }
+    // console.log(req.files.file)
+
+    const file = req.files.file
+
+    //Make sure image is a photo
+    if (!file.mimetype.startsWith('image')) {
+        return next(
+           new ErrorResponse(`Please upload an image file`, 400)
+        )
+    }
+
+    //CHeck file size
+    if (file.size > process.env.MAX_FILE_UPLOAD) {
+        return next(
+            new ErrorResponse(`Please upload an image file less tha ${process.env.MAX_FILE_UPLOAD}`, 400)
+        )
+    }
+
+    //Create custom filename
+    file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+        if (err) {
+            console.error(err)
+            return next(
+                new ErrorResponse(`Problem with file upload`, 500)
+            )
+        }
+
+        await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+
+        res.status(200).json({
+            success: true,
+            data: file.name
+        })
+    })
+
+    console.log(file.name)
+})
