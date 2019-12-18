a57f78524f9e1ac97c13d70cd90788117da251d0
kmyles82 <kmyles82@gmail.com>
2019-12-18 17:10:07 -0600

    added bcrypt to hash user password in database and create user

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -25,11 +27,17 @@ const UserSchema = new mongoose.Schema({
         select: false,
         resetPasswordToken: String,
         resetPasswordExpire: Date,
-        createAt: {
-            type: Date,
-            default: Date.now
-        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
+});
+
+//Encrypt password using bcrypt
+UserSchema.pre('save', async function (nex) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 })
 
 module.exports = mongoose.model('User', UserSchema);
